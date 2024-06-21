38aa0b7b8de15e8177a70bdb3d5c26d73c2d4689
Rameez <shaikhrameez516@gmail.com>
2024-06-21 16:20:13 +0530

    more patterns

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,33 +2,19 @@ public class Palindrome {
     // palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards,
     // such as madam or racecar,
     public static void main(String[] args) {
-        int x=1231;
-        System.out.println(isPalindrome(x));
+        String a = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(a));
 
 
     }
-//    static boolean isPalindrome(String str){
-//        str=str.toLowerCase();
-//        for(int i=0;i<str.length()/2;i++){
-//            char start = str.charAt(i);
-//            char end = str.charAt(str.length()-1-i);
-//            if (start != end ){
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-static boolean isPalindrome(int x) {
-    String a= String.valueOf(x);
-    for(int i =0;i<a.length()/2;i++){
-        char start = a.charAt(i);
-        char end = a.charAt(a.length()-1-i);
-        if (start != end ){
-            return false;
+    static boolean isPalindrome(String s){
+        s=s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        int n=s.length();
+        for(int i=0;i<n/2;i++){
+            if (s.charAt(i) != s.charAt(n-1-i)) return false;
         }
+        return true;
     }
-    return true;
-
-}
 
 }
--- src/patterns/patterns.java
+++ src/patterns/patterns.java
@@ -2,7 +2,7 @@ package patterns;
 
 public class patterns {
     public static void main(String[] args) {
-        pattern4(5);
+        pattern31(5);
 
     }
     static void pattern1(int n){
@@ -38,11 +38,73 @@ public class patterns {
     static void pattern4(int n){
         for (int row=1;row<=n;row++){
             for (int col=1;col<=row;col++){
-                System.out.print(row);
+                System.out.print(col);
             }
             System.out.println();
         }
 
 
     }
+    static void pattern5(int n){
+        for (int row=0;row< 2*n;row++){
+            int totalcol = row > n ? 2 * n - row : row;
+            for (int col=0;col<totalcol;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+    static void pattern28(int n){
+        for (int row=0;row< 2*n;row++){
+
+            int totalcol = row > n ? 2 * n - row : row;
+            int spaces= n-totalcol;
+            for (int s=0;s<spaces;s++){
+                System.out.print(" ");
+            }
+            for (int col=0;col<totalcol;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+    static void pattern30(int n){
+        for (int row=1;row< n;row++){
+
+
+            for (int s=0;s<n-row;s++){
+                System.out.print(" ");
+            }
+            for (int col=row;col>=1;col--){
+                System.out.print(col);
+            }
+            for (int col=2;col<=row;col++){
+                System.out.print(col);
+            }
+            System.out.println();
+
+        }
+
+
+    }
+    static void pattern31(int n){
+        int a=n;
+        n=2*n;
+        for(int row=0;row<=n;row++){
+
+            for(int col=0;col<=n;col++){
+                int atEveryIndex=a-Math.min(Math.min(row,col),Math.min(n-row,n-col));
+                System.out.print(atEveryIndex+" ");
+
+            }
+            System.out.println();
+        }
+    }
+
 }
